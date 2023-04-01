cef7b835403ffd6866d953ad1840f1c5f4487c83
Retr0 <mihaildrumev@gmail.com>
2023-04-01 14:40:05 +0300

    ready

---
+++ Project_MusicO/MusicO/MusicO/MusicO/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MusicO.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MusicO.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Project_MusicO/MusicO/MusicO/MusicO/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MusicO.Models;
+using MusicO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MusicO.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
